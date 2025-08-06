dca9118543a4db2eabff5d286edca41fe0c31630
kaleabmit <kalabmitku0@gmail.com>
2025-08-07 01:58:56 +0300

    updated remote database configuration with railway database platform

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,35 @@
-import { Module } from '@nestjs/common';
+// import { Module } from '@nestjs/common'; 
+// import { ProductModule } from './product/product.module';
+// import { ConfigModule } from '@nestjs/config';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+// import { CategoryModule } from './category/category.module';
+
+// @Module({
+//   imports: [
+//     ConfigModule.forRoot({
+//       isGlobal: true,
+//     }),
+//     TypeOrmModule.forRoot({
+//     type:'mysql',
+//     database:'products',
+//     port:3306,
+//     host:'localhost',
+//     username:'root',
+//     password:'Mysql@@@82987932',
+//     autoLoadEntities:true,
+//     synchronize:true
+//     }),
+//     ProductModule,
+//     CategoryModule,
+//   ],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+// export class AppModule {}
+
+import { Module } from '@nestjs/common'; 
 import { ProductModule } from './product/product.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -13,11 +44,11 @@ import { CategoryModule } from './category/category.module';
     }),
     TypeOrmModule.forRoot({
     type:'mysql',
-    database:'products',
-    port:3306,
-    host:'localhost',
+    database:'railway',
+    port:31564,
+    host:'mainline.proxy.rlwy.net',
     username:'root',
-    password:'Mysql@@@82987932',
+    password:'CHMMvHZYBJAKmWupSIYONytDqQwXbiTC',
     autoLoadEntities:true,
     synchronize:true
     }),
